5834b957c0de896161d42737f5a9360f4437b4f4
Provolone Elease <pelease@oa-i5.21-school.ru>
2020-01-22 22:06:17 +0300

    starting intExecution

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "include/ft_printf.h"
+
+int				main(void)
+{
+	int			a;
+
+	a = 5;
+	printf("%-0d", 123);
+	//ft_printf("%123+-d", a);
+	return (0);
+}
---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "libft/libft.h"
+
+int	main()
+{
+	const char a[] = "asb";
+	//printf("%d\n", atoi("abc123"));
+	//		printf("%d\n", atoi("qwe.3"));
+	//printf("%10qdbc\n", 20);
+	printf("%llhhlhd\n", 9223372036854775807);
+	return 0;
+}
